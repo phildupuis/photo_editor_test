ca9d2ab48130468138dc9ca241235721bfa1f019
Philippe Dupuis <philippe@braver.health>
2024-02-10 16:55:32 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/photo_editor_test/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.photo_editor_test
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
